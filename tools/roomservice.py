4a02c6527ab01482b726a2fa0df3e18e8c7334ce
Adam Farden <adam@farden.cz>
2015-11-27 20:08:02 +0100

    Check the 'CyanogenMod' remote for its default revision
    Check the default revision of the first remote defined in our
    manifest (eg base-l-cm.xml) which is 'CyanogenMod' instead of
    the overall 'default' revision which is an AOSP tag sync'd from
    CAF.
    
    Signed-off-by: Adam Farden <adam@farden.cz>

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -111,7 +111,7 @@ def indent(elem, level=0):
 
 def get_default_revision():
     m = ElementTree.parse(".repo/manifest.xml")
-    d = m.findall('default')[0]
+    d = m.findall('remote')[0]
     r = d.get('revision')
     return r.replace('refs/heads/', '').replace('refs/tags/', '')
 
